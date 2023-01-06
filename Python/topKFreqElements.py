20d5bb72215061121b8630eb01a2fb46e8e4917c
raman1383 <raman83shariati@gmail.com>
2023-01-06 16:37:47 +0330

    t

--- Python/topKFreqElements.py
+++ Python/topKFreqElements.py
@@ -1,1 +1,15 @@
-# def topKFrequentElements(nums: list[list[int]], k: int) -> list[int]:
+def topKFrequentElements(nums: list[list[int]], k: int) -> list[int]:
+    count = {}
+    freq = [[]for i in range(len(range(nums))+1)]
+
+    for n in nums:
+        count[n] = count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
+    res = []
+    for i in range(len(freq)-1, 0, -1):
+        for n in freq[i]:
+            res.append(n)
+            if len(res) == k:
+                return res
